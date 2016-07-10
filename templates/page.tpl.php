786c10db320cc4fb3195dae968cd977166a898c1
Roald Umandal <umandalroald@gmail.com>
2016-07-10 11:09:53 +0800

    update page tpl

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -212,42 +212,31 @@
           </li>
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <!-- The user image in the navbar-->
+              <!-- <img src="/profiles/bms/themes/bms_adminlte/dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> -->
+              <?php print $avatarsm; ?>
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs"><?php print $fullname; ?></span>
             </a>
             <ul class="dropdown-menu">
-              <!-- User image -->
+              <!-- The user image in the menu -->
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
+                <!-- <img src="/profiles/bms/themes/bms_adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"> -->
+                <?php print $avatar; ?>
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  <?php print $fullname; ?>  - <?php print $role; ?>
+                  <small><?php print $history; ?></small>
                 </p>
-              </li>
-              <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <a href="/<?php print $profile; ?>" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="<?php print $logout; ?>" class="btn btn-default btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
@@ -322,7 +311,7 @@
 
     </section><!-- /.content -->
   </div><!-- /.content-wrapper -->
-  
+
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
