a5dbf62d82c13f8e6cb6a0c664d7bf889fe0e415
Roald Umandal <umandalroald@gmail.com>
2016-07-13 22:39:26 +0800

    add description in layout option appearance

--- theme-settings.php
+++ theme-settings.php
@@ -22,6 +22,7 @@ function adminlte_form_system_theme_settings_alter(&$form, $form_state) {
   $form['adminlte_group']['layout_group'] = array(
     '#type' => 'fieldset',
     '#title' => t('Layout Options'),
+    '#description' => t('Enable AdminLTE layout'),
     '#weight' => -20,
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
