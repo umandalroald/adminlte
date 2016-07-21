50707e954f9737c1fe2a33be730c9f783693cc30
Roald Umandal <umandalroald@gmail.com>
2016-07-22 00:06:13 +0800

    add comment at top of function

--- theme-settings.php
+++ theme-settings.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Implement hook_form_system_theme_settings_alter()
+ */
 function adminlte_form_system_theme_settings_alter(&$form, $form_state) {
 
   $form['adminlte_group'] = array(
